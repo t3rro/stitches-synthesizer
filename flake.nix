cc2beb91c78496f38962e8eb8b891488387e9d94
t3rro <t3rro@protonmail.com>
2023-02-22 09:26:10 -0800

    init

--- flake.nix
+++ flake.nix
@@ -5,37 +5,33 @@
   inputs.ruby-flake-utils.url = "github:t3rro/ruby-flake-utils";
 
   outputs = { self, nixpkgs, flake-utils, ruby-flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        name = "stitches-synthesizer";
-        system = "x86_64-linux";
-        gems = pkgs.bundlerEnv configurations.bundlerConfig;
-        pkgs = import nixpkgs { inherit system; };
-        rflutils = ruby-flake-utils.lib;
-        funcs = rflutils.mkFuncs pkgs bins;
-        scripts = rflutils.mkScripts funcs;
-        envs = rflutils.mkEnvs pkgs configurations;
-        bins = rflutils.mkBins envs pkgs;
-        configurations = rflutils.mkConfigurations name pkgs envs scripts bins {
-          lockfile = ./Gemfile.lock;
-          gemfile = ./Gemfile;
-          gemset = ./gemset.nix;
-        };
-      in
-      rec {
-        packages = flake-utils.lib.flattenTree {
-          stitches = pkgs.callPackage ./package.nix { inherit pkgs; };
-        };
-        defaultPackage = packages.stitches;
-        devShell =
+    let
+      systems = flake-utils.lib.eachDefaultSystem
+        (
+          system:
           let
-            derivationConfig = configurations.derivationConfig // {
-              shellHook = "zsh";
-              buildInputs = configurations.derivationConfig.buildInputs ++ [ pkgs.zsh ];
-            };
+            gemSystem = ruby-flake-utils.lib.mkGemSystem system "stitches-synthesizer";
+
+            inherit (gemSystem) pkgs configurations;
+
+            thisSystem =
+              rec {
+                packages = flake-utils.lib.flattenTree {
+                  stitches = pkgs.callPackage ./package.nix { inherit pkgs; };
+                };
+                defaultPackage = packages.stitches;
+                devShell =
+                  let
+                    derivationConfig = configurations.derivationConfig // {
+                      shellHook = "zsh";
+                      buildInputs = configurations.derivationConfig.buildInputs ++ [ pkgs.zsh ];
+                    };
+                  in
+                  pkgs.stdenv.mkDerivation derivationConfig;
+              };
           in
-          pkgs.stdenv.mkDerivation derivationConfig;
-      }
-    );
+          thisSystem
+        );
+    in
+    systems;
 }
