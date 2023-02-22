f4f4d46a5c11f4e13de78ced3d5512909c67804c
t3rro <t3rro@protonmail.com>
2023-02-22 10:05:27 -0800

    remove some scripts

--- flake.nix
+++ flake.nix
@@ -1,14 +1,11 @@
-let
-  name = "stitches-synthesizer";
-in
 {
-  description = name;
+  description = "stitches-synthesizer";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs;
   inputs.ruby-flake-utils.url = github:t3rro/ruby-flake-utils;
 
   outputs = { ruby-flake-utils, nixpkgs, ... }: ruby-flake-utils.lib.mkGemSystems
-    name
+    "stitches-synthesizer"
     nixpkgs
     ./Gemfile.lock
     ./Gemfile
