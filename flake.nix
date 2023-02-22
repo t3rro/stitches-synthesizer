8a3ac0f468f62aae59b58547f974d6f3a2a4d41b
t3rro <t3rro@protonmail.com>
2023-02-22 12:03:28 -0800

    fix foldl

--- flake.nix
+++ flake.nix
@@ -4,11 +4,12 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs;
   inputs.ruby-flake-utils.url = github:t3rro/ruby-flake-utils;
 
-  outputs = { ruby-flake-utils, nixpkgs, ... }: ruby-flake-utils.lib.mkGemSystems
-    "stitches-synthesizer"
-    nixpkgs
-    ./Gemfile.lock
-    ./Gemfile
-    ./gemset.nix
+  outputs = { ruby-flake-utils, nixpkgs, ... }:
+    ruby-flake-utils.lib.mkGemSystems
+      "stitches-synthesizer"
+      nixpkgs
+      ./Gemfile.lock
+      ./Gemfile
+      ./gemset.nix
   ;
 }
