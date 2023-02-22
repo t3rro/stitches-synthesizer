e026e1d4d3e0921e80d1696234b0b20b947b4290
t3rro <t3rro@protonmail.com>
2023-02-22 08:06:41 -0800

    init

--- flake.nix
+++ flake.nix
@@ -2,15 +2,18 @@
   description = "stitches synthesizer";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.ruby-flake-utils.url = "github:t3rro/stitches-synthesizer";
 
-  outputs = { self, nixpkgs, flake-utils, }:
+  outputs = { self, nixpkgs, flake-utils, ruby-flake-utils }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        name = "stitches-synthesizer";
         system = "x86_64-linux";
         gems = pkgs.bundlerEnv common.bundlerConfig;
-        common = import ./common.nix { inherit pkgs; };
         pkgs = import nixpkgs { inherit system; };
+        gemDefaults = ruby-flake-utils.lib.mkGemDefaults { inherit name pkgs; };
+        configurations = gemDefaults.configurations;
       in
       rec {
         packages = flake-utils.lib.flattenTree {
@@ -19,9 +22,9 @@
         defaultPackage = packages.stitches;
         devShell =
           let
-            derivationConfig = common.derivationConfig // {
+            derivationConfig = configurations.derivationConfig // {
               shellHook = "zsh";
-              buildInputs = common.derivationConfig.buildInputs ++ [ pkgs.zsh ];
+              buildInputs = configurations.derivationConfig.buildInputs ++ [ pkgs.zsh ];
             };
           in
           pkgs.stdenv.mkDerivation derivationConfig;
