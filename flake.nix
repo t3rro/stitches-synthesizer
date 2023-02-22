578efd66c2eeb9b43c46deb7e527eecde05dbbdb
t3rro <t3rro@protonmail.com>
2023-02-22 10:00:13 -0800

    remove some scripts

--- flake.nix
+++ flake.nix
@@ -6,36 +6,37 @@
 
   outputs = { self, nixpkgs, flake-utils, ruby-flake-utils }@attrs:
     let
-      systems = flake-utils.lib.eachDefaultSystem
-        (
-          system:
-          let
-            gemSystem = ruby-flake-utils.lib.mkGemSystem
-              system
-              "stitches-synthesizer"
-              nixpkgs
-              ./Gemfile.lock
-              ./Gemfile
-              ./gemset.nix;
-
-            # inherit (gemSystem) pkgs configurations;
-            #
-            # thisSystem =
-            #   rec {
-            #     packages = flake-utils.lib.flattenTree { stitches = pkgs.stdenv.mkDerivation configurations.derivationConfig; };
-            #     defaultPackage = packages.stitches;
-            #     devShell =
-            #       let
-            #         derivationConfig = configurations.derivationConfig // {
-            #           shellHook = "zsh";
-            #           buildInputs = configurations.derivationConfig.buildInputs ++ [ pkgs.zsh ];
-            #         };
-            #       in
-            #       pkgs.stdenv.mkDerivation derivationConfig;
-            #   };
-          in
-          gemSystem
-        );
+      systems = ruby-flake-utils.lib.mkGemSystems "stitches-synthesizer" nixpkgs ./Gemfile.lock ./Gemfile ./gemset.nix;
+      # systems = flake-utils.lib.eachDefaultSystem
+      #   (
+      #     system:
+      #     let
+      #       gemSystem = ruby-flake-utils.lib.mkGemSystem
+      #         system
+      #         "stitches-synthesizer"
+      #         nixpkgs
+      #         ./Gemfile.lock
+      #         ./Gemfile
+      #         ./gemset.nix;
+      #
+      #       # inherit (gemSystem) pkgs configurations;
+      #       #
+      #       # thisSystem =
+      #       #   rec {
+      #       #     packages = flake-utils.lib.flattenTree { stitches = pkgs.stdenv.mkDerivation configurations.derivationConfig; };
+      #       #     defaultPackage = packages.stitches;
+      #       #     devShell =
+      #       #       let
+      #       #         derivationConfig = configurations.derivationConfig // {
+      #       #           shellHook = "zsh";
+      #       #           buildInputs = configurations.derivationConfig.buildInputs ++ [ pkgs.zsh ];
+      #       #         };
+      #       #       in
+      #       #       pkgs.stdenv.mkDerivation derivationConfig;
+      #       #   };
+      #     in
+      #     gemSystem
+      #   );
     in
     systems;
 }
