31b563d19ba80fb59445f7f6c7a848f6591f5564
t3rro <t3rro@protonmail.com>
2023-02-23 13:46:23 -0800

    c flake.nix

--- flake.nix
+++ flake.nix
@@ -5,11 +5,13 @@
   inputs.ruby-flake-utils.url = github:t3rro/ruby-flake-utils;
 
   outputs = { ruby-flake-utils, nixpkgs, ... }:
-    ruby-flake-utils.lib.mkGemSystems
-      "stitches-synthesizer"
-      nixpkgs
-      ./Gemfile.lock
-      ./Gemfile
-      ./gemset.nix
-  ;
+    ruby-flake-utils.lib.mkGemSystems {
+      inherit nixpkgs;
+      name = "stitches-synthesizer";
+      lockfile = ./Gemfile.lock;
+      gemfile = ./Gemfile;
+      gemset = ./gemset.nix;
+      strategy = "lib";
+      src = ./.;
+    };
 }
